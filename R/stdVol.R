be41c9638cac6074de2536cd9ef126bdc963fd16
Sasha D. Hafner <saha@kbm.sdu.dk>
2015-05-28 00:00:00 +0000

    version 1.0.1

--- R/stdVol.R
+++ R/stdVol.R
@@ -1,4 +1,4 @@
-# Modified: 5 MAR 2015
+# Modified: 1 MAY 2015 SDH
 
 stdVol <-
 function(vol,temp=0,pres=1,rh=1,temp.std=NULL,pres.std=NULL,unit.pres="atm",unit.temp="C") {
@@ -6,7 +6,7 @@ function(vol,temp=0,pres=1,rh=1,temp.std=NULL,pres.std=NULL,unit.pres="atm",unit
   pres<-unitConvert(x=pres,unit=unit.pres,to='atm')
   temp<-unitConvert(x=temp,unit=unit.temp,to='C')
 
-  # Next two lines avoid problems when unit.__ is changed but __.norm isn't
+  # Next two lines avoid problems when unit.__ is changed but __.std isn't
   if(is.null(pres.std)) pres.std<-1.0 else pres.std<-unitConvert(x=pres.std,unit=unit.pres,to='atm')
   if(is.null(temp.std)) temp.std<-0.0 else temp.std<-unitConvert(x=temp.std,unit=unit.temp,to='C')
   
@@ -20,7 +20,7 @@ function(vol,temp=0,pres=1,rh=1,temp.std=NULL,pres.std=NULL,unit.pres="atm",unit
   vol.dry<-vol*(pres - pH2O)/pres.std
 
   # Correct dry volume for temperature
-  vol.norm<-vol.dry*(temp.std + 273.15)/(temp + 273.15)
+  vol.std<-vol.dry*(temp.std + 273.15)/(temp + 273.15)
 
-  return(vol.norm)
+  return(vol.std)
 }
--- R/summBg.R
+++ R/summBg.R
@@ -1,3 +1,4 @@
+# Modified: 5 May 2015 SDH and CRE
 summBg <-
 function(vol,
          setup,
@@ -32,71 +33,125 @@ function(vol,
     names(setup)[1:2]<-c(id.name,descrip.name)
   } 
 
+  # Find reactor/bottle IDs present in both vol and setup
+  ids<-intersect(setup[,id.name],vol[,id.name])
+
   # Interpolate cvCH4 to common time for each reactor
-  s1<-data.frame(id=ids<-unique(setup[,id.name]))
-  names(s1)[1]<-id.name
+  summ1<-expand.grid(id=ids,time=when)
+  names(summ1)<-c(id.name,time.name)
+
   if(class(when) %in% c('numeric','integer')) {
     # Then interpolate
     for(i in ids) {
       dc<-vol[vol[,id.name]==i,]
-      s1[s1[,id.name]==i,vol.name]<-interp(dc[,time.name],dc[,vol.name],time.out=when,method=imethod,extrap=extrap)
+      # Interpolate if more than one value is present
+      if(nrow(dc)>1) {
+        summ1[summ1[,id.name]==i,vol.name]<-interp(dc[,time.name],dc[,vol.name],time.out=when,method=imethod,extrap=extrap)
+      } else {
+	if(dc[,time.name]==when) { # `when` argument matches the single time present
+          summ1[summ1[,id.name]==i,vol.name]<-dc[,vol.name]
+	} else {
+          summ1[summ1[,id.name]==i,vol.name]<-NA
+      	  warning('There is only a single ',vol.name,' value for reactor ',i,', and it does not match the specified when (',when,'). Interpolation is not possible.')
+	}
+      }
     }
   } else if(tolower(when)=='end') { # User just wants to use latest values of volume
     # Sort, in order to find latest values
     vol<-vol[order(vol[,id.name],vol[,vol.name]),]
     for(i in ids) {
       dc<-vol[vol[,id.name]==i,]
-      s1[s1[,id.name]==i,vol.name]<-dc[nrow(dc),vol.name]
+      summ1[summ1[,id.name]==i,vol.name]<-dc[nrow(dc),vol.name]
     }
   }
   # Merge to add mass inoculum and VS in substrate
-  s1<-merge(setup,s1,by=id.name)
+  summ1<-merge(setup,summ1,by=id.name)
 
-  # Pull out inoculum data
-  if(!is.null(inoc.name) && inoc.name %in% setup[,descrip.name]) {
+  # Messages about inoculum and normalization
+  # If these if statements ever change be sure to change the IDENTICAL statements below
+  if(!is.null(inoc.name) && inoc.name %in% setup[,descrip.name]) { # Inoculum contribution subtracted
     message('Inoculumn contribution subtracted based on setup$',inoc.m.name,'.') 
-    is1<-s1[s1[,descrip.name]==inoc.name,]
-    is1$vol.mi<-is1[,vol.name]/is1[,inoc.m.name]
-    inocvol<-c(mn=mean(is1$vol.mi),s=sd(is1$vol.mi))
-
-    # Remaining data
-    s1<-s1[s1[,descrip.name]!=inoc.name,]
-    if(is.factor(s1[,id.name])) s1[,id.name]<-droplevels(s1[,id.name])
-
-    # Calculate and substract inoc contribution
-    s1[,vol.name]<-s1[,vol.name] - inocvol['mn']*s1[,inoc.m.name]
-    s1[,'sd.inoc']<-inocvol['s']*s1[,inoc.m.name]
-
   } else {
-    message('Inoculumn contribution not subtracted.') 
-    s1[,'sd.inoc']<-0
+      message('Inoculumn contribution not subtracted.') 
   }
 
-  if(!is.null(norm.name)) {
-    message('CH4 production normalized by setup$',norm.name,'.')
-    # Normalize remaining vol by norm.name (typically by substrate VS)
-    s1[,vol.name]<-s1[,vol.name]/s1[,norm.name]
-    s1[,'sd.inoc']<-s1[,'sd.inoc']/s1[,norm.name]
+  if(!is.null(norm.name)) { # Normalization
+    message('Response normalized by setup$',norm.name,'.')
   } else {
     message('No normalization by substrate mass.')
   }
 
-  if(show.obs) {
-    s1$sd.inoc<-NULL
-    s1<-s1[order(s1[,descrip.name],s1[,id.name]),]
-    return(s1)
+  # Loop through all times within when argument
+  # NTS: there may be a more efficient way to do this
+  summ2<-data.frame()
+  for (w in when) {
+    s1<-summ1[summ1[,time.name]==w,]
+
+    # Pull out inoculum data
+    if(!is.null(inoc.name) && inoc.name %in% setup[,descrip.name]) { # Inoculum contribution subtracted
+      # Subset with just inoculum data
+      is1<-s1[s1[,descrip.name]==inoc.name,]
+
+      # Volume contribution per unit inoculum mass
+      is1$vol.mi<-is1[,vol.name]/is1[,inoc.m.name]
+
+      # Mean and sd volume contribution per unit inoc mass
+      inocvol<-c(mn=mean(is1$vol.mi),s=sd(is1$vol.mi))
+
+      # Remaining data (drop out inoculum data)
+      s1<-s1[s1[,descrip.name]!=inoc.name,]
+      if(is.factor(s1[,id.name])) s1[,id.name]<-droplevels(s1[,id.name])
+
+      # Calculate and substract inoc contribution
+      # Next three lines only for returning additional info when show.obs=TRUE
+      s1[,paste0(vol.name,'.tot')]<-s1[,vol.name]
+      s1[,paste0(vol.name,'.inoc')]<-inocvol['mn']*s1[,inoc.m.name]
+      s1[,'fv.inoc']<-s1[,paste0(vol.name,'.inoc')]/s1[,paste0(vol.name,'.tot')]
+
+      # Correct vol for inoculum
+      s1[,vol.name]<-s1[,vol.name] - inocvol['mn']*s1[,inoc.m.name]
+
+      # Add sd in volume produced by inoculum for use below in error propagation
+      s1[,'sd.inoc']<-inocvol['s']*s1[,inoc.m.name]
+
+    } else {
+      s1[,'sd.inoc']<-0
+    }
+
+    if(!is.null(norm.name)) { 
+      # Normalize remaining vol by norm.name (typically by substrate VS)
+      s1[,vol.name]<-s1[,vol.name]/s1[,norm.name]
+
+      # Normalize sd contribution from inoc by the same value
+      s1[,'sd.inoc']<-s1[,'sd.inoc']/s1[,norm.name]
+
+      # Next two lines only for returning additional info when show.obs=TRUE
+      # Only have the .tot and .inoc columns when inoc is subtracted out
+      if(!is.null(inoc.name) && inoc.name %in% setup[,descrip.name]) { 
+        s1[,paste0(vol.name,'.tot')]<-s1[,paste0(vol.name,'.tot')]/s1[,norm.name]
+        s1[,paste0(vol.name,'.inoc')]<-s1[,paste0(vol.name,'.inoc')]/s1[,norm.name]
+      }
+    } 
+
+    # Calculate means and sd for a summary
+    if(!show.obs) {
+      # Summarize by description
+      s2<-data.frame(descrip=unique(s1[,descrip.name]),time=w)
+      names(s2)<-c(descrip.name,time.name)
+      for(i in unique(s1[,descrip.name])){
+        dd<-s1[s1[,descrip.name]==i,]
+        s2[s2[,descrip.name]==i,'mean']<-signif(mean(na.omit(dd[,vol.name])),5)
+        #s2[s2[,descrip.name]==i,'sd']<-signif(sd(na.omit(dd[,vol.name])),5) # Original code with no contribution of inoc to sd
+        s2[s2[,descrip.name]==i,'sd']<-signif(sqrt(sd(na.omit(dd[,vol.name]))^2 + mean(dd[,'sd.inoc'])^2),5) # NTS: Probably there is a better way to do this
+        s2[s2[,descrip.name]==i,'n']<-sum(!is.na(dd[,vol.name]))  
+      }
+    } else { # If show.obs=TRUE, just return individual observations
+      #s1$sd.inoc<-NULL
+      s2<-s1[order(s1[,descrip.name],s1[,id.name]),]
+    }
+
+    summ2<-rbind(summ2,s2)
   }
 
-  # Summarize by description
-  s2<-data.frame(descrip=unique(s1[,descrip.name]))
-  names(s2)[1]<-descrip.name
-  for(i in unique(s1[,descrip.name])){
-    dd<-s1[s1[,descrip.name]==i,]
-    s2[s2[,descrip.name]==i,'mean']<-signif(mean(na.omit(dd[,vol.name])),5)
-    #s2[s2[,descrip.name]==i,'sd']<-signif(sd(na.omit(dd[,vol.name])),5) # Original code with no contribution of inoc to sd
-    s2[s2[,descrip.name]==i,'sd']<-signif(sqrt(sd(na.omit(dd[,vol.name]))^2 + mean(dd[,'sd.inoc'])^2),5) # NTS: Probably there is a better way to do this
-    s2[s2[,descrip.name]==i,'n']<-sum(!is.na(dd[,vol.name]))  
-  }
-
-  return(s2)
+  return(summ2)
 }
